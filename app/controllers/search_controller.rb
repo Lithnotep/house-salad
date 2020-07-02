3e36199dd6a9cdd12de3f699c35d928ebc9ee181
Max Mitrani <mjmitrani168@gmail.com>
2020-07-02 11:46:58 -0600

    add refactor using a poros PropublicaData to pull data then bring it over to the search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,15 @@
 class SearchController < ApplicationController
   def index
+    @members = PropublicaData.new.response(params[:state])
+    # data = PropublicaData.new
+    # @members = data.response(params[:state])
+    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+    #   faraday.headers["X-API-KEY"] = ENV["PROPUBLICA_API_KEY"]
+    # end
+    #
+    # response = conn.get("/congress/v1/members/house/#{state}/current.json")
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @members = json[:results]
+
   end
 end
